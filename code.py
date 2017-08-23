fc1eee9d7d10c65d1ec7be55b052f8eab571eecb
runnerbeany <nah>
2017-08-23 23:54:30 +0100

    no message

--- code.py
+++ code.py
@@ -4,7 +4,15 @@
 import logging, discord, datetime, json, sys
 from discord.ext import commands
 import conf
+from colorama import init, Fore, Back, Style
 
+
+
+print("""Initialising Colorama...
+
+""")
+init()
+print(Style.BRIGHT + Back.BLUE + Fore.RED + 'Color' + Fore.GREEN + 'ama' + Style.RESET_ALL + ' initialised!')
 token = conf.token
 #DEBUG = i want to hear the sound of the air moving as a pin drops
 #INFO = i want to hear a pin drop
@@ -27,8 +35,8 @@ logger.addHandler(stdout_handler)
 @bot.event
 async def on_ready():
     bot.start_time = datetime.datetime.now()
-    print('Bot started at ', bot.start_time)
-    print("Logged in as {} with ID {}".format(bot.user.name, bot.user.id))
+    print('Bot started at ', Style.BRIGHT, Fore.YELLOW, bot.start_time, Style.RESET_ALL)
+    print("Logged in as {} with ID {}".format(bot.user.name+'#'+bot.user.discriminator, bot.user.id))
     users = sum(1 for user in bot.get_all_members())
     channels = sum(1 for channel in bot.get_all_channels())
     print("Serving {} users in {} channels on {} guilds".format(users, channels, len(bot.servers)))
@@ -42,17 +50,20 @@ async def source(ctx):
 
 @bot.event
 async def on_server_join(guild):
-    logging.info('GUILD_CREATE: {0.name} <{0.id}>'.format(guild))
+
+    print(Style.BRIGHT, Fore.CYAN+ 'GUILD_CREATE: {0.name} <{0.id}>'.format(guild) + Style.RESET_ALL)
 
 @bot.event
 async def on_server_remove(guild):
-    logging.info('GUILD_REMOVE: {0.name} (id {0.id})'.format(guild))
+
+    print(Style.BRIGHT, Fore.RED + 'GUILD_REMOVE: {0.name} (id {0.id})'.format(guild) + Style.RESET_ALL)
 
 @bot.command(pass_context=True)
 async def shutdown(ctx):
     await bot.reply("I'm shutting down now.")
     game = discord.Game(name='Shutting down...')
     await bot.change_presence(status=discord.Status.dnd)
+    print(Back.RED, Style.BRIGHT, "Bot shutting down by request of [this should say author]")
     await bot.logout()
 
 
